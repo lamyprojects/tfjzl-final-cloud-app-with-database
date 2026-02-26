034f857592d244b92874a14715350b9e51810bc1
lamyprojects <lamynessa23@gmail.com>
2026-02-26 13:23:23 +0000

    save

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,18 +1,7 @@
-"""myproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+myproject URL Configuration
+"""
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
@@ -20,5 +9,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('onlinecourse/', include('onlinecourse.urls')),
+
+    # ovo omogućava da početna stranica / otvara onlinecourse app
+    path('', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
